655d7dd9c52b3e99eeca2c37587241e067e4d3dd
Ehsan Karimi <ehsan.karimi.cryp@gmail.com>
2024-04-01 00:22:22 +0330

    header added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { BrowserRouter , Routes , Route} from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home"
+
 function App() {
-  return <h1>Text</h1>;
+  return (
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    
+    </BrowserRouter>
+  )
 }
 
 export default App;
