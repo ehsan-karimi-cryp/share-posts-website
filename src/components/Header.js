f26ce1c6f7a094d59b7541a0f2489b59c58a4047
Ehsan Karimi <ehsan.karimi.cryp@gmail.com>
2024-04-01 10:54:59 +0330

    home page designed with bootstrap and css

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,20 +5,20 @@ function Header(){
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <NavLink className="navbar-brand" to="/">Share Posts</NavLink>
+    <NavLink className={x => x.isActive ? "navbar-brand active" : "navbar-brand"} to="/">Share Posts</NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
+          <NavLink className={x => x.isActive ? "nav-link active" : "nav-link"} aria-current="page" to="/">Home</NavLink>
         </li>
         <li className="nav-item">
           <NavLink className="nav-link" to="/users">Users</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink className="nav-link" to="/posts">Posts</NavLink>
+          <NavLink className={x => x.isActive ? "nav-link active" : "nav-link"} to="/posts">Posts</NavLink>
         </li>
       </ul>
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,33 @@
-function Home(){
-    return <h3>Home</h3>
+import { Link } from "react-router-dom";
 
+function Home() {
+  return (
+    <section className="container ">
+      <div className="row m-5 justify-content-center">
+        <div className="col-12 col-md-6 justify-content-center website-title">
+          <h3>Share Posts</h3>
+        </div>
+      </div>
+      <div className="row m-3 justify-content-center">
+        <div className="col-12 col-md-6">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+            distinctio possimus recusandae iure magni minus debitis nam nisi
+            dolores beatae? Tempora voluptas reprehenderit facilis recusandae
+            ducimus aspernatur, possimus eaque ipsum nam voluptate, nihil quo
+            obcaecati accusantium dolorum asperiores similique corrupti
+            laboriosam minima odio. Quidem consectetur, nam quis earum iusto
+            tenetur.
+          </p>
+        </div>
+      </div>
+      <div className="row m-3 justify-content-center">
+        <div className="col-12 col-md-6 buttons-section">
+          <Link className="btn btn-info page-button" to="/users">Users</Link>
+          <Link className="btn btn-info page-button" to="/posts">Posts</Link>
+        </div>
+      </div>
+    </section>
+  );
 }
 export default Home;
