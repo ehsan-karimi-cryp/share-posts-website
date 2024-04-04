8a543823436765a8d699fcf905e07d1392f1bfbb
Ehsan Karimi <ehsan.karimi.cryp@gmail.com>
2024-04-04 17:44:50 +0330

    create post feature and sweet alert added

--- src/pages/posts/CreatePost.js
+++ src/pages/posts/CreatePost.js
@@ -1,12 +1,45 @@
 import { useState } from "react";
+import Swal from "sweetalert2";
 
 function CreatePost() {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const submitForm = (x) => {
     x.preventDefault();
-    console.log(title, body);
+    setLoading(true);
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        title: title,
+        body: body,
+        userId: 1,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setLoading(false);
+        Swal.fire({
+          title: "Thanks!",
+          text: "Post created successfully",
+          icon: "success",
+          confirmButtonText: "OK",
+        });
+      })
+      .catch((err) => {
+        setLoading(false);
+        Swal.fire({
+          title: "Error",
+          text: err.message,
+          icon: "error",
+          confirmButtonText: "OK",
+        });
+      });
   };
   return (
     <>
@@ -22,6 +55,9 @@ function CreatePost() {
                   className="form-control"
                   onChange={(x) => setTitle(x.target.value)}
                 />
+                <div className="form-text text-danger">
+                  {title ? "" : "Title is required"}
+                </div>
               </div>
               <div className="mb-3">
                 <label className="form-label">Body:</label>
@@ -30,8 +66,23 @@ function CreatePost() {
                   rows="3"
                   onChange={(x) => setBody(x.target.value)}
                 ></textarea>
+                <div className="form-text text-danger">
+                  {body ? "" : "Body is required"}
+                </div>
               </div>
-              <button type="submit" className="btn btn-dark">
+              <button
+                type="submit"
+                className="btn btn-dark"
+                disabled={title === "" || body === ""}
+              >
+                {loading && (
+                  <div
+                    className="spinner-border spinner-border-sm me-2"
+                    role="status"
+                  >
+                    <span className="visually-hidden">Loading...</span>
+                  </div>
+                )}
                 Create
               </button>
             </form>
